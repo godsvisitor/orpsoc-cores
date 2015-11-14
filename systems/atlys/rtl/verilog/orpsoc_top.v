fd16fd2c30be9fb000cb2139813e914a6eb32f2a
Mehmet Tukel <mehmettukel@gmail.com>
2015-11-15 01:50:03 +0200

    Change boot address

--- systems/atlys/rtl/verilog/orpsoc_top.v
+++ systems/atlys/rtl/verilog/orpsoc_top.v
@@ -233,7 +233,7 @@ assign or1k_rst = wb_rst | or1k_dbg_rst;
 
 `ifdef OR1200
 
-or1200_top #(.boot_adr(32'hf0000100))
+or1200_top #(.boot_adr(32'hf0000000))
 or1200_top0 (
 	// Instruction bus, clocks, reset
 	.iwb_clk_i			(wb_clk),
@@ -331,7 +331,7 @@ mor1kx #(
 	.IBUS_WB_TYPE("B3_REGISTERED_FEEDBACK"),
 	.DBUS_WB_TYPE("B3_REGISTERED_FEEDBACK"),
 	.OPTION_CPU0("CAPPUCCINO"),
-	.OPTION_RESET_PC(32'hf0000100)
+	.OPTION_RESET_PC(32'hf0000000)
 ) mor1kx0 (
 	.iwbm_adr_o(wb_m2s_or1k_i_adr),
 	.iwbm_stb_o(wb_m2s_or1k_i_stb),
